8edae90fc3624206d0841daa8bfbe71426216831
ShipIM <ilya.shipunov@gmail.com>
2023-10-14 17:02:23 +0300

    user added

---
+++ university-scb/security/src/main/java/com/university/security/user/User.java
@@ -0,0 +1,27 @@
+package com.university.security.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+}
