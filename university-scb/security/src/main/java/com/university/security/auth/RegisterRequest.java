412c257dbf12069481a2ae6b9b09037866d6bc9e
ShipIM <ilya.shipunov@gmail.com>
2023-10-14 20:23:30 +0300

    security base

---
+++ university-scb/security/src/main/java/com/university/security/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.university.security.auth;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+}
