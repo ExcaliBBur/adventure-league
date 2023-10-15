82f2afc612baa82e2f42fdaddcd25ee0916d6843
Roman <roman_kravets_den@mail.ru>
2023-10-16 02:35:30 +0300

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import {MainPage} from "./components/mainPage/MainPage";
 import IndexContent from "./components/indexContent/IndexContent";
 import "./style.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <IndexContent />
-           {/*<MainPage />*/}
-        </div>
+    <div className="App">
+        <h1>Application</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />}> </Route>
+                <Route path="/login" element={<IndexContent />}> </Route>
+            </Routes>
+        </BrowserRouter>
+    </div>
     );
 }
 
