a58f7b052bedfd4e6a402df113d763bf274bc1cb
Alexander <excalibburn@shinra-tensei.ru>
2023-10-16 19:52:39 +0300

    edit config files

--- university-scb/demo/src/main/java/com/university/demo/DemoController.java
+++ university-scb/demo/src/main/java/com/university/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.university.demo;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
